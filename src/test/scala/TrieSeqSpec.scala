d0702c54dee1359e66b03dd51558eeed8b9c9489
sullivan- <john.sullivan.mscs@gmail.com>
2014-02-16 17:08:35 -0500

    initial version of TrieSeq
    Acked-by: sullivan- <john.sullivan.mscs@gmail.com>

---
+++ src/test/scala/TrieSeqSpec.scala
@@ -0,0 +1,46 @@
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers
+
+/** Spec-based tests for ArrayBuffer. */
+class TrieSeqSpec extends FlatSpec with Matchers {
+
+  behavior of "TrieSeq"
+  it should "throw IndexOutOfBoundsException as appropriate" in {
+    var ts = TrieSeq[Int]()
+    intercept[IndexOutOfBoundsException] {
+      ts(-1)
+    }
+    intercept[IndexOutOfBoundsException] {
+      ts(0)
+    }
+    ts = ts.append(1)
+    intercept[IndexOutOfBoundsException] {
+      ts(-1)
+    }
+    ts(0)
+    intercept[IndexOutOfBoundsException] {
+      ts(1)
+    }
+    ts = ts.append(2)
+    intercept[IndexOutOfBoundsException] {
+      ts(-1)
+    }
+    ts(0)
+    ts(1)
+    intercept[IndexOutOfBoundsException] {
+      ts(2)
+    }
+  }
+
+  behavior of "TrieSeq"
+  it should "work as advertised for short sequences" in {
+    var ts = TrieSeq[Int]()
+    ts = ts.append(1)
+    ts = ts.append(2)
+    ts = ts.append(4)
+    ts(0) should equal { 1 }
+    ts(1) should equal { 2 }
+    ts(2) should equal { 4 }
+  }
+
+}
